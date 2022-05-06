9f184d56bfe2de7dbd5fd5bcf632bf35085f8feb
Juliano Lorenzato <julianolorenzato@gmail.com>
2022-05-05 21:02:01 -0300

    m

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from './swagger.json'
 
 import { router } from './routes'
 
@@ -9,7 +7,6 @@ import './shared/infra/typeorm'
 const app = express()
 
 app.use(express.json())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(router)
 
